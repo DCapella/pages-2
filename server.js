10326664d05ea314f99cc1f8111a21c83463d6d2
David Capella <D_Capella@yahoo.com>
2018-04-03 10:38:39 -0500

    finished project

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongo = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+const db = require('./config/db');
+
+const app = express();
+
+const port = 8000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongo.connect(db.url, (err, database) => {
+  if (err) return console.log(err);
+  let db = database.db('pages');
+  require('./app/routes')(app, db);
+
+  app.listen(port, () => {
+    console.log("We are live on " + port);
+  });
+});
